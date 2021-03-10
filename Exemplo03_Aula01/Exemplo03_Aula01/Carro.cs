e03989e6f375a2c353037000feb4046350f48b05
Alexandre <alexandre.vaz@gmail.com>
2021-03-10 12:12:54 -0300

    Conclusão da primeira parte.

---
+++ Exemplo03_Aula01/Exemplo03_Aula01/Carro.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo03_Aula01
+{
+    class Carro
+    {
+        // Atributos
+        public string modelo;
+        public string cor;
+        public int anoFabricacao;
+        public int velocidadeMaxima;
+        public int velocidadeAtual;
+        public bool seLigado;
+
+        // Construtores
+        public Carro(string modelo, string cor, int anoFabricacao, int velocidadeMaxima)
+        {
+            this.modelo = modelo;
+            this.cor = cor;
+            this.anoFabricacao = anoFabricacao;
+            this.velocidadeMaxima = velocidadeMaxima;
+            this.velocidadeAtual = 0;
+            this.seLigado = false;
+        }
+
+        public Carro() { }
+
+        // Métodos
+        public void Ligar()
+        {
+            this.seLigado = true;
+            Console.WriteLine("Carro ligado!!");
+            Console.ReadLine();
+        }
+
+        public void Acelerar()
+        {
+            this.velocidadeAtual = this.velocidadeAtual + 10;
+        }
+
+        public void Freiar()
+        {
+            this.velocidadeAtual = this.velocidadeAtual - 10;
+        }
+
+    }
+}
