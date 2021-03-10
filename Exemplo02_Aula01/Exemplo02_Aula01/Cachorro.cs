6692581270b545c1b835541762f125b6df120320
Alexandre <alexandre.vaz@gmail.com>
2021-03-10 11:37:37 -0300

    Concluido

---
+++ Exemplo02_Aula01/Exemplo02_Aula01/Cachorro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo02_Aula01
+{
+    class Cachorro
+    {
+        public string nome;
+        public string raca;
+        public int idade;
+        public string genero;
+
+        public void AbanarRabo()
+        {
+            Console.WriteLine("Rabinho balançando!!!");
+            Console.ReadLine();
+        }
+
+        public void Latir()
+        {
+            Console.WriteLine("Au au au");
+            Console.ReadLine();
+        }
+    }
+}
